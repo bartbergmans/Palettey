48d0ee353b1aff14cc6f215377ec36b21e50981e
George Ciesinski <george.ciesin@gmail.com>
2023-09-30 15:26:24 -0400

    Added two new tests that are copies of the original tests but with missing hashes

--- test/index.test.js
+++ test/index.test.js
@@ -39,3 +39,43 @@ it("generates a luminance palette based on a color", () => {
     },
   });
 });
+
+it("generates a palette based on a color from value missing hash", () => {
+  expect(
+    palettey.createPaletteFromColor("primary", "7953e0", {})
+  ).toMatchObject({
+    primary: {
+      50: "#F1EEFC",
+      100: "#E4DCF9",
+      200: "#C9BAF3",
+      300: "#AE97EC",
+      400: "#9375E6",
+      500: "#7953E0",
+      600: "#5325CF",
+      700: "#3E1C9C",
+      800: "#291368",
+      900: "#150934",
+    },
+  });
+});
+
+it("generates a luminance palette based on a color from value missing hash", () => {
+  expect(
+    palettey.createPaletteFromColor("primary", "7953e0", {
+      useLightness: false,
+    })
+  ).toMatchObject({
+    primary: {
+      50: "#F8F6FD",
+      100: "#EEE9FB",
+      200: "#DACFF6",
+      300: "#C2B1F1",
+      400: "#A78FEB",
+      500: "#7953E0",
+      600: "#6E45DE",
+      700: "#5A2BD9",
+      800: "#4A21BA",
+      900: "#341782",
+    },
+  });
+});
